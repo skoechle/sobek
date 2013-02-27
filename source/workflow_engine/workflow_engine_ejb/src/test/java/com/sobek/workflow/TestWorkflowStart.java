5e9e83c6ebe9b6f08fb64154c03f3fff822b4a76
ericbrumlow <eric_brumlow@qwest.net>
2013-02-26 20:38:20 -0700

    added start of workflow unit testing

---
+++ source/workflow_engine/workflow_engine_ejb/src/test/java/com/sobek/workflow/TestWorkflowStart.java
@@ -0,0 +1,21 @@
+package com.sobek.workflow;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+
+
+public class TestWorkflowStart {
+
+	@Rule
+	public ExpectedException expected = ExpectedException.none();
+
+	@Test
+	public void nullParameter() {
+		WorkflowBean bean = new WorkflowBean();
+
+		expected.expect(IllegalArgumentException.class);
+		bean.start(null);
+	}
+}
